dd6ce20f8017016b18b63707f77c2b6730c6f15b
mananx22 <motorollaman27@gmail.com>
2023-01-31 08:34:03 -0800

    2 terraform ec2 instance resource added

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-#   access_key = $AWS_ACCESS_KEY_ID
-#   secret_key = $AWS_SECRET_ACCESS_KEY 
+  #   access_key = $AWS_ACCESS_KEY_ID
+  #   secret_key = $AWS_SECRET_ACCESS_KEY 
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-052465340e6b59fc0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "TerraformBasicMachine"
+  }
 }
