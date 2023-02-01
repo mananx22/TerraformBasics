89300f79f5d3ed19440498367754410c545594f6
mananx22 <motorollaman27@gmail.com>
2023-01-31 23:13:07 -0800

    6 Using terraform count

--- main.tf
+++ main.tf
@@ -15,44 +15,30 @@ provider "aws" {
 }
 
 resource "random_string" "mananrandom" {
-  length           = 6
-  special          = false
-  override_special = false
-}
-
-resource "random_string" "mananrandom2" {
+  count = 3
   length           = 6
   special          = false
   override_special = false
 }
 
 resource "aws_instance" "web" {
+  count = 3
   ami           = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
   tags = {
-    Name = join("-", ["TerraformBasicMachine" , random_string.mananrandom.result])
+    Name = join("-", ["TerraformBasicMachine" , random_string.mananrandom[count.index].result])
   }
 }
 
-resource "aws_instance" "web2" {
-  ami           = "ami-01a4f99c4ac11b03c"
-  instance_type = "t2.micro"
-  tags = {
-    Name = join("-", ["TerraformBasicMachine" , random_string.mananrandom2.result])
-  }
+ output "Instance-ami" {
+  value = aws_instance.web[*].ami
 }
 
-output "instance_ami_using" {
-  value = aws_instance.web.ami
+ output "Instance-Ip" {
+  value = aws_instance.web[*].public_ip
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-  description = "public ip of aws ec2 instance"
-}
-
- output "Instance-Details" {
-  value = join( " : " ,["ami used is", aws_instance.web.ami,"  & volume size (GB) is", aws_instance.web.root_block_device[0].volume_size])
-  description = "public ip of aws ec2 instance"
+ output "Instance-volume" {
+  value = aws_instance.web[*].root_block_device[0].volume_size
 }
  
